429271bdda851775fa7aaf34b0fd567c1d61028c
Bob Cao <bobcaocheng@gmail.com>
2020-03-18 23:10:03 -0700

    Upload

---
+++ src/pathtracer/advanced_bsdf.cpp
@@ -0,0 +1,101 @@
+#include "bsdf.h"
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+
+
+using std::max;
+using std::min;
+using std::swap;
+
+namespace CGL {
+
+// Mirror BSDF //
+
+Spectrum MirrorBSDF::f(const Vector3D& wo, const Vector3D& wi) {
+  return Spectrum();
+}
+
+Spectrum MirrorBSDF::sample_f(const Vector3D& wo, Vector3D* wi, float* pdf) {
+  // TODO:
+  // Implement MirrorBSDF
+  return Spectrum();
+}
+
+// Microfacet BSDF //
+
+double MicrofacetBSDF::G(const Vector3D& wo, const Vector3D& wi) {
+  return 0.0;
+}
+
+double MicrofacetBSDF::D(const Vector3D& h) {
+  // TODO: proj3-2, part 3
+  // Compute Beckmann normal distribution function (NDF) here.
+  // You will need the roughness alpha.
+  return 0.0;
+}
+
+Spectrum MicrofacetBSDF::F(const Vector3D& wi) {
+  // TODO: proj3-2, part 3
+  // Compute Fresnel term for reflection on dielectric-conductor interface.
+  // You will need both eta and etaK, both of which are Spectrum.
+  return Spectrum();
+}
+
+Spectrum MicrofacetBSDF::f(const Vector3D& wo, const Vector3D& wi) {
+  // TODO: proj3-2, part 3
+  // Implement microfacet model here.
+  return Spectrum();
+}
+
+Spectrum MicrofacetBSDF::sample_f(const Vector3D& wo, Vector3D* wi, float* pdf) {
+  // TODO: proj3-2, part 3
+  // *Importance* sample Beckmann normal distribution function (NDF) here.
+  // Note: You should fill in the sampled direction *wi and the corresponding *pdf,
+  //       and return the sampled BRDF value.
+  return Spectrum();
+}
+
+// Refraction BSDF //
+
+Spectrum RefractionBSDF::f(const Vector3D& wo, const Vector3D& wi) {
+  return Spectrum();
+}
+
+Spectrum RefractionBSDF::sample_f(const Vector3D& wo, Vector3D* wi, float* pdf) {
+  // TODO:
+  // Implement RefractionBSDF
+  return Spectrum();
+}
+
+// Glass BSDF //
+
+Spectrum GlassBSDF::f(const Vector3D& wo, const Vector3D& wi) {
+  return Spectrum();
+}
+
+Spectrum GlassBSDF::sample_f(const Vector3D& wo, Vector3D* wi, float* pdf) {
+  // TODO:
+  // Compute Fresnel coefficient and use it as the probability of reflection
+  // - Fundamentals of Computer Graphics page 305
+  return Spectrum();
+}
+
+void BSDF::reflect(const Vector3D& wo, Vector3D* wi) {
+  // TODO:
+  // Implement reflection of wo about normal (0,0,1) and store result in wi.
+
+}
+
+bool BSDF::refract(const Vector3D& wo, Vector3D* wi, float ior) {
+  // TODO:
+  // Use Snell's Law to refract wo surface and store result ray in wi.
+  // Return false if refraction does not occur due to total internal reflection
+  // and true otherwise. When dot(wo,n) is positive, then wo corresponds to a
+  // ray entering the surface through vacuum.
+
+  return true;
+}
+
+} // namespace CGL
